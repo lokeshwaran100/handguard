be228e01da1f6fd1c066439b3d36c3fc93240aa2
Lokeshwaran B <lokeshwaran100@gmail.com>
2025-09-28 01:20:44 +0530

    Update package.json and yarn.lock for dependency management; upgrade package manager to Yarn 4.10.3 and remove outdated dependencies from devDependencies.

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -18,7 +18,7 @@ const providerApiKey = process.env.ALCHEMY_API_KEY || "oKxs-03sij-U_N0iOlrSsZFr2
 // If not set, it uses the hardhat account 0 private key.
 // You can generate a random account with `yarn generate` or `yarn account:import` to import your existing PK
 const deployerPrivateKey =
-  process.env.__RUNTIME_DEPLOYER_PRIVATE_KEY ?? "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+  process.env.__RUNTIME_DEPLOYER_PRIVATE_KEY ?? "0x8ef39e2ed4638705c4a0c3218eb75e2e91e2b3bb23dc39daab627b3c7f12202d";
 // If not set, it uses our block explorers default API keys.
 const etherscanApiKey = process.env.ETHERSCAN_V2_API_KEY || "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW";
 
