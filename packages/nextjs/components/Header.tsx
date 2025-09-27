17a331ed134a1c5deb1644997ae20cb51778d581
Adithya2310 <Adithyanandi099@gmail.com>
2025-09-28 03:00:50 +0530

    Updated the logo

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -90,7 +90,7 @@ export const Header = () => {
         </details>
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
           <div className="flex relative w-10 h-10">
-            <Image alt="SE-2 logo" className="cursor-pointer" fill src="/logo.jpeg" />
+            <Image alt="SE-2 logo" className="cursor-pointer" fill src="/logo.png" />
           </div>
           <div className="flex flex-col">
             <span className="font-bold leading-tight">Handguard Index</span>
